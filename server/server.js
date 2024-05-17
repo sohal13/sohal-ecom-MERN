facf2858735e3b662b128b0852b36533d92b82f1
sohal13 <sohalr1305@gmail.com>
2024-05-17 23:58:55 +0530

    #

--- server/server.js
+++ server/server.js
@@ -26,12 +26,13 @@ app.use('/api/stripe',stripeRout)
 
 app.use(express.static(path.join(__dirname, `/client/dist`)));
 
-/*app.get('*',(req,res)=>{
-    res.sendFile(path.join(__dirname,'client','dist','index.html'))
-})*/
 app.get('*',(req,res)=>{
-    res.send({message:"Server is Wokring", success:true})
+    res.sendFile(path.join(__dirname,'client','dist','index.html'))
 })
+
+/*app.get('*',(req,res)=>{
+    res.send({message:"Server is Wokring", success:true})
+})*/
 
 const PORT = process.env.PORT || 4040
 app.listen(PORT,()=>{
