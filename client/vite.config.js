9467e99f71cd5e42bc9f437b9be93e2352abaf7d
sohal rahaman <sohalr1305@gmail.com>
2024-05-30 00:38:01 +0530

    #

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,12 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import dotenv from 'dotenv';
 
+dotenv.config();
 // https://vitejs.dev/config/
 export default defineConfig({
+
   server: {
     proxy: {
       '/api': {
-        target:'https://sohal-ecom-mern-backend.onrender.com',
+        target:process.env.VITE_API_BASE_URL,
         changeOrigin: true,
       },
     },
