75bfc509131cc39e12806f4bee9b3aaa3fa676a0
sohal rahaman <sohalr1305@gmail.com>
2024-05-30 02:11:10 +0530

    #

--- client/vite.config.js
+++ client/vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target:process.env.VITE_API_BASE_URL,
+        target:'http://localhost:4040',
         changeOrigin: true,
       },
     },
